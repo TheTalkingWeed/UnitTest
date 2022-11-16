aa568484ad14fd2268d1bb023c9e89bfad51bf70
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2022-11-16 16:15:42 +0200

    Add files via upload

---
+++ 02_myutils_duplaz_strlen/Main.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Math myMath = new Math();
+        // myMath.max(3, 6);
+
+        TestUtils.testDuplaz();
+        TestUtils.testStrlen();
+        TestUtils.testAbs();
+        TestUtils.testMax();
+
+        System.out.println(Math.abs(-5));
+        System.out.println(Math.max(20, 10));
+
+        System.out.println(MyUtils.duplaz(6));
+        System.out.println(MyUtils.strlen("hellóka"));
+    }
+}
---
+++ 02_myutils_duplaz_strlen/TestUtils.java
@@ -0,0 +1,37 @@
+public class TestUtils {
+
+    public static void testDuplaz(){
+        assert MyUtils.duplaz(2)  == 4: "Helyes";
+        assert MyUtils.duplaz(4)  == 8: "Helyes";
+        assert MyUtils.duplaz(7)  == 14: "Helyes";
+        assert MyUtils.duplaz(0)  == 0: "Helyes";
+    }
+    public static void testStrlen(){
+        assert MyUtils.strlen("szoveg") == 6 : "Helyes";
+        assert MyUtils.strlen("hello") == 5 : "Helyes";
+        assert MyUtils.strlen("alma") == 4 : "Helyes";
+        assert MyUtils.strlen("animal") == 6 : "Helyes";
+        assert MyUtils.strlen("programming") == 11 : "Helyes";
+    }
+
+    public static void testAbs(){
+        assert Math.abs(-5) == 5 : "Helyes";
+        assert Math.abs(-6) == 6 : "Helyes";
+        assert Math.abs(12) == 12 : "Helyes";
+        assert Math.abs(-56) == 56 : "Helyes";
+        assert Math.abs(125) == 125 : "Helyes";
+
+    }
+    public static void testMax(){
+
+        assert Math.max(12,10) == 12 : "Helyes";
+        assert Math.max(13,0) == 13 : "Helyes";
+        assert Math.max(31,44) == 44 : "Helyes";
+        assert Math.max(234,623) == 623 : "Helyes";
+        assert Math.max(123,5) == 123 : "Helyes";
+
+
+
+    }
+
+}
